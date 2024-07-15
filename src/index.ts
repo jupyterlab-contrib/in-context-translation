dbc3f37e02c8940994ae97921547e4cec2e83909
Frédéric Collonval <fcollonval@gmail.com>
2024-07-15 14:39:57 +0200

    Load the crowdin in-context script (#1)
    * Load the crowdin in-context script
    
    * Fix yarn.lock and first install
    
    * Update the documentation
    
    * Prettify
    
    ---------
    
    Co-authored-by: Frédéric Collonval <fcollonval@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,28 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
+import { ITranslator } from '@jupyterlab/translation';
 /**
  * Initialization data for the @jupyterlab/in-context-translation extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: '@jupyterlab/in-context-translation:plugin',
-  description: 'A Jupyter extension to translate the web interface directly within the application.',
+  description:
+    'A Jupyter extension to translate the web interface directly within the application.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension @jupyterlab/in-context-translation is activated!');
+  requires: [ITranslator],
+  activate: async (app: JupyterFrontEnd, translator: ITranslator) => {
+    console.log(
+      'JupyterLab extension @jupyterlab/in-context-translation is activated!'
+    );
+
+    if (translator.languageCode === 'ach_UG') {
+      // @ts-expect-error Unknown _jipt variable
+      window._jipt = [['project', 'jupyterlab']];
+      // @ts-expect-error Unknown file - as it is download from cdn at build time
+      const content = await import('./jipt.js');
+      eval(content);
+    }
   }
 };
 
